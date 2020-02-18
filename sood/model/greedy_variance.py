7b9115029e65c34cd707f973844486bd74bf6494
zicun cong <congzicun@gmail.com>
2020-02-18 11:22:34 -0800

    Test greedy variance

--- sood/model/greedy_variance.py
+++ sood/model/greedy_variance.py
@@ -4,6 +4,8 @@
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+from tqdm import tqdm
+
 from sood.data_process.data_loader import DataLoader, Dataset
 from pyod.utils.stat_models import wpearsonr
 from sood.log import getLogger
@@ -11,36 +13,9 @@ from scipy.stats import spearmanr
 from sood.model.abs_model import AbstractModel, Aggregator
 import numpy as np
 
+from sood.model.base_detectors import kNN
+
 logger = getLogger(__name__)
-
-
-def jaccard(g_array, l_array):
-    g_array = g_array[:150]
-    l_array = l_array[:150]
-    g_set = set(g_array.tolist())
-    l_set = set(l_array.tolist())
-    return len(g_set & l_set) / len(g_set | l_set)
-
-
-def spearman(g_array, l_array):
-    g_rank = [0] * g_array.shape[0]
-    l_rank = [0] * l_array.shape[0]
-
-    for rank, idx in enumerate(g_array):
-        g_rank[idx] = rank
-    for rank, idx in enumerate(l_array):
-        l_rank[idx] = rank
-    return spearmanr(g_rank, l_rank)[0]
-
-
-def pearson(initial_rank_list, local_rank_list):
-    global_outlying_idx = np.argsort(initial_rank_list)[::-1]
-    local_outlying_idx = np.argsort(local_rank_list)[::-1]
-    weights = [0] * initial_rank_list.shape[0]
-    for rank, idx in enumerate(global_outlying_idx):
-        weights[idx] = 1 / (rank + 1)
-    logger.info(f"Score {initial_rank_list[global_outlying_idx[0]]} {initial_rank_list[global_outlying_idx[1]]}")
-    return wpearsonr(initial_rank_list, local_outlying_idx, weights)
 
 
 class GreedyVariance(AbstractModel):
@@ -74,22 +49,22 @@ class GreedyVariance(AbstractModel):
             # Randomly sample feature size
             feature_size = np.random.randint(self.dim_start, self.dim_end)
             # Randomly select features
-            selected_features = np.random.choice(feature_index, feature_size)
+            selected_features = np.random.choice(feature_index, feature_size, p=feature_w)
             _X = data_array[:, selected_features]
             # Process selected dataset
             local_rank_list = self.mdl.fit(_X)
 
             if len(model_outputs):
-                roc_auc = mdl.compute_roc_auc(np.array(self.aggregate_components(model_outputs)), Y)
+                roc_auc = self.compute_roc_auc(np.array(self.aggregate_components(model_outputs)), self.Y)
                 print("Ensemble Before {}".format(roc_auc))
 
-            local_roc = mdl.compute_roc_auc(np.array(self.aggregate_components([local_rank_list, ])), Y)
+            local_roc = self.compute_roc_auc(np.array(self.aggregate_components([local_rank_list, ])), self.Y)
             print(f"Local {local_roc}")
             rocs.append(local_roc)
             if local_roc > self.threshold:
                 counter += 1
                 model_outputs.append(local_rank_list)
-                roc_auc = mdl.compute_roc_auc(np.array(self.aggregate_components(model_outputs)), Y)
+                roc_auc = self.compute_roc_auc(np.array(self.aggregate_components(model_outputs)), self.Y)
                 print("Ensemble After {}".format(roc_auc))
             print('-' * 50)
         logger.info("Number of good subspace {}/{}".format(counter, self.ensemble_size))
@@ -108,7 +83,7 @@ class GreedyVariance(AbstractModel):
             return Aggregator.average_threshold(model_outputs, 2)
 
 
-def test():
+def test_threshold():
     for dataset in [Dataset.ARRHYTHMIA, Dataset.OPTDIGITS, Dataset.MUSK, Dataset.MNIST_ODDS]:
         for aggregator in [Aggregator.COUNT_STD_THRESHOLD, Aggregator.AVERAGE_THRESHOLD]:
             for threshold in [0, 0.5, 0.7, 0.9]:
@@ -127,18 +102,35 @@ def test():
                 except Exception as e:
                     logger.exception(e)
 
+
+def test_greedy_threshold():
+    for dataset in [Dataset.ARRHYTHMIA, Dataset.OPTDIGITS, Dataset.MUSK, Dataset.MNIST_ODDS]:
+        for aggregator in [Aggregator.COUNT_STD_THRESHOLD, Aggregator.AVERAGE_THRESHOLD, Aggregator.AVERAGE, Aggregator.COUNT_RANK_THRESHOLD]:
+            for threshold in [0, 0.5, 0.7, 0.9]:
+                X, Y = DataLoader.load(dataset)
+                dim = X.shape[1]
+                neigh = max(10, int(np.floor(0.03 * X.shape[0])))
+                ENSEMBLE_SIZE = 100
+                logger.info(f"{dataset} {aggregator} {threshold}")
+                roc_aucs = []
+                precision_at_ns = []
+                for _ in tqdm.trange(5):
+                    mdl = GreedyVariance(1, dim / 2, ENSEMBLE_SIZE, aggregator, neigh, kNN.NAME, Y, threshold)
+                    try:
+                        rst = mdl.run(X)
+                        roc_auc = mdl.compute_roc_auc(rst, Y)
+                        logger.info("Final ROC {}".format(roc_auc))
+                        precision_at_n = mdl.compute_precision_at_n(rst, Y)
+                        logger.info("Precision@n {}".format(precision_at_n))
+
+                        roc_aucs.append(roc_auc)
+                        precision_at_ns.append(precision_at_n)
+                    except Exception as e:
+                        logger.exception(e)
+                logger.info(f"Exp Information {dataset} {aggregator} {threshold}")
+                logger.info(f"Final Average ROC {np.mean(roc_aucs)}")
+                logger.info(f"Final Precision@n {np.mean(precision_at_ns)}")
+                logger.info(f"====================================================")
+
 if __name__ == '__main__':
-    from sood.model.base_detectors import kNN
-    X, Y = DataLoader.load(Dataset.OPTDIGITS)
-    dim = X.shape[1]
-    neigh = max(10, int(np.floor(0.03 * X.shape[0])))
-    ENSEMBLE_SIZE = 100
-    mdl = GreedyVariance(1, dim / 2, ENSEMBLE_SIZE, Aggregator.AVERAGE, neigh, kNN.NAME, Y, 0)
-    try:
-        rst = mdl.run(X)
-        roc_auc = mdl.compute_roc_auc(rst, Y)
-        logger.info("Final ROC {}".format(roc_auc))
-        precision_at_n = mdl.compute_precision_at_n(rst, Y)
-        logger.info("Precision@n {}".format(precision_at_n))
-    except Exception as e:
-        logger.exception(e)
+    test_greedy_threshold()
