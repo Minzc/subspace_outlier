b760f6a53bea32e664484ad81186233d3e7bb2bd
Minzc <congzicun@gmail.com>
2020-02-18 21:47:42 -0800

    Add vertical select

---
+++ sood/model/select_v.py
@@ -0,0 +1,177 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+#
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+import numpy as np
+from pyod.utils import wpearsonr
+from scipy.stats import spearmanr
+
+from sood.model.base_detectors import LOF, kNN
+import time
+from sood.model.abs_model import AbstractModel, Aggregator
+from sood.log import getLogger
+
+# ====================================================
+# Feature Bagging
+# - Uniform sampling low dimensional data
+# ====================================================
+
+logger = getLogger(__name__)
+
+def spearman(targe_rank_list, local_rank_list):
+    global_outlying_idx = np.argsort(targe_rank_list)[::-1]
+    local_outlying_idx = np.argsort(local_rank_list)[::-1]
+
+    g_rank = [0] * global_outlying_idx.shape[0]
+    l_rank = [0] * global_outlying_idx.shape[0]
+
+    for rank, idx in enumerate(global_outlying_idx):
+        g_rank[idx] = rank
+    for rank, idx in enumerate(local_outlying_idx):
+        l_rank[idx] = rank
+    return spearmanr(g_rank, l_rank)[0]
+
+def pearson(targe_rank_list, local_rank_list):
+    global_outlying_idx = np.argsort(targe_rank_list)[::-1]
+    local_outlying_idx = np.argsort(local_rank_list)[::-1]
+    weights = [0] * targe_rank_list.shape[0]
+    for rank, idx in enumerate(global_outlying_idx):
+        weights[idx] = 1 / (rank + 1)
+    logger.info(f"Score {targe_rank_list[global_outlying_idx[0]]} {targe_rank_list[global_outlying_idx[1]]}")
+    return wpearsonr(targe_rank_list, local_outlying_idx, w=weights)
+
+
+class SelectV(AbstractModel):
+    NAME = "SelectV"
+    def __init__(self, dim_start, dim_end, ensemble_size, aggregate_method, neighbor, base_model):
+        name = f"FB({dim_start}-{dim_end} Neighbor: {neighbor}))"
+        super().__init__(name, aggregate_method, base_model, neighbor)
+        self.dim_start = dim_start
+        self.dim_end = dim_end
+        self.ensemble_size = ensemble_size
+        self.aggregate_method = aggregate_method
+        np.random.seed(1)
+
+    def model_selection(self, model_outputs):
+        target_list = np.array(self.aggregate_components(model_outputs))
+        selected_models = []
+        model_outputs = [(i, pearson(target_list, i)) for i in model_outputs]
+        model_outputs = list(sorted(model_outputs, key=lambda i: i[1], reverse=True))
+        selected_models.append(model_outputs.pop(0)[0])
+        model_outputs = [i[0] for i in model_outputs]
+
+        while len(model_outputs) > 0:
+            selected_rank_list = np.array(self.aggregate_components(selected_models))
+            current_spearman = pearson(selected_rank_list, target_list)
+            model_outputs = list(sorted([(i, pearson(selected_rank_list, i)) for i in model_outputs],
+                                        key=lambda x: x[1], reverse=True))
+            model_outputs = [i[0] for i in model_outputs]
+            model_output = model_outputs.pop(0)
+            selected_models.append(model_output)
+            selected_rank_list = np.array(self.aggregate_components(selected_models))
+            new_spearman = pearson(selected_rank_list, target_list)
+            if new_spearman <= current_spearman:
+                selected_models = selected_models[:-1]
+        print(f"Number of selected models {len(selected_models)}")
+        return selected_models
+
+
+
+    def compute_ensemble_components(self, data_array):
+        model_outputs = []
+        feature_index = np.array([i for i in range(data_array.shape[1])])
+        for i in range(self.ensemble_size):
+            # Randomly sample feature size
+            feature_size = np.random.randint(self.dim_start, self.dim_end)
+            # Randomly select features
+            selected_features = np.random.choice(feature_index, feature_size)
+            logger.debug(f"Feature size: {feature_size}")
+            logger.debug(f"Selected feature: {selected_features}")
+            _X = data_array[:, selected_features]
+            logger.debug(f"Selected X: {_X.shape}")
+            # Process selected dataset
+            score = self.mdl.fit(_X)
+            model_outputs.append(score)
+            logger.debug(f"Outlier score shape: {score.shape}")
+        model_outputs = self.model_selection(model_outputs)
+
+        return model_outputs
+
+    def aggregate_components(self, model_outputs):
+        if self.aggregate_method == Aggregator.COUNT_RANK_THRESHOLD:
+            return Aggregator.count_rank_threshold(model_outputs, 0.05)
+        elif self.aggregate_method == Aggregator.AVERAGE:
+            return Aggregator.average(model_outputs)
+        elif self.aggregate_method == Aggregator.COUNT_STD_THRESHOLD:
+            return Aggregator.count_std_threshold(model_outputs, 2)
+        elif self.aggregate_method == Aggregator.AVERAGE_THRESHOLD:
+            return Aggregator.average_threshold(model_outputs, 2)
+
+
+if __name__ == '__main__':
+    from sood.data_process.data_loader import Dataset, DataLoader
+
+    ENSEMBLE_SIZE = 100
+    EXP_NUM = 1
+    PRECISION_AT_N = 10
+
+    X, Y = DataLoader.load(Dataset.ARRHYTHMIA)
+    dim = X.shape[1]
+    neigh = max(10, int(np.floor(0.03 * X.shape[0])))
+
+    # for start, end in [ (1, int(dim / 2)), (int(dim / 2), dim)]:
+    for start, end in [ (1, int(dim / 2)), ]:
+        selectv = SelectV(start, end, ENSEMBLE_SIZE, Aggregator.AVERAGE_THRESHOLD, neigh, kNN.NAME)
+
+        start_ts = time.time()
+        roc_aucs = []
+        precision_at_ns = []
+
+        for i in range(EXP_NUM):
+            rst = selectv.run(X)
+
+            logger.debug(f"Ensemble output {rst}")
+            logger.debug(f"Y {Y}")
+
+            roc_auc = selectv.compute_roc_auc(rst, Y)
+            roc_aucs.append(roc_auc)
+
+            precision_at_n = selectv.compute_precision_at_n(rst, Y, PRECISION_AT_N)
+            precision_at_ns.append(precision_at_n)
+
+        end_ts = time.time()
+        logger.info(
+            f""" Model: {selectv.info()} ROC AUC {np.mean(roc_aucs)} Std: {np.std(roc_aucs)} Precision@n {np.mean(precision_at_ns)} Std: {np.std(precision_at_ns)} Time Elapse: {end_ts - start_ts}""")
+
+    # logger.info("=" * 50)
+    # for start, end in [(4 * int(dim / 10), 5 * int(dim / 10)),
+    #                    (3 * int(dim / 10), 4 * int(dim / 10)),
+    #                    (2 * int(dim / 10), 3 * int(dim / 10)),
+    #                    (1, 2 * int(dim / 10)),
+    #                    (1, int(dim / 10)),
+    #                    (1, int(dim / 2)),
+    #                    (int(dim / 2), dim)]:
+    #     fb = FB(start, end, ENSEMBLE_SIZE, Aggregator.AVERAGE, neigh, kNN.NAME)
+    #
+    #     start_ts = time.time()
+    #     roc_aucs = []
+    #     precision_at_ns = []
+    #
+    #     for i in range(EXP_NUM):
+    #         rst = fb.run(X)
+    #
+    #         logger.debug(f"Ensemble output {rst}")
+    #         logger.debug(f"Y {Y}")
+    #
+    #         roc_auc = fb.compute_roc_auc(rst, Y)
+    #         roc_aucs.append(roc_auc)
+    #
+    #         precision_at_n = fb.compute_precision_at_n(rst, Y, PRECISION_AT_N)
+    #         precision_at_ns.append(precision_at_n)
+    #
+    #     end_ts = time.time()
+    #     logger.info(
+    #         f""" Model: {fb.info()} ROC AUC {np.mean(roc_aucs)} Std: {np.std(roc_aucs)} Precision@n {np.mean(precision_at_ns)} Std: {np.std(precision_at_ns)} Time Elapse: {end_ts - start_ts}""")
+    #
+    # logger.info("Finish")
