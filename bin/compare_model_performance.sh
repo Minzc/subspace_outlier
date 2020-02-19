d132c6bf9e4514826cdc4d0d41050fe608d5a6ce
zicun cong <congzicun@gmail.com>
2020-02-19 15:33:01 -0800

    bug fix

--- bin/compare_model_performance.sh
+++ bin/compare_model_performance.sh
@@ -1,5 +1,5 @@
-source venv/bin/active
+source venv/bin/activate
 cd sood
-python exp/compare_model_performance.py -m fb > log/feature_bagging_batch_test_20200219.log
+nohup python exp/compare_model_performance.py -m fb > log/feature_bagging_batch_test_20200219.log &
 
 
