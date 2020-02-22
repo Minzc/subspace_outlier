3b7dbe7b4fcb91ad49b0776a0cfdfb1ce651c847
zicun cong <congzicun@gmail.com>
2020-02-21 16:45:17 -0800

    Add model choice

--- sood/exp/enumerate_features.py
+++ sood/exp/enumerate_features.py
@@ -151,6 +151,7 @@ def subspace_count_per_point():
     import json
     BIN_NUM = 10
     outputs = defaultdict(dict)
+    model = 'knn'
 
     for dataset in [Dataset.VOWELS, Dataset.WINE,
                     Dataset.BREASTW, Dataset.ANNTHYROID,
@@ -161,9 +162,12 @@ def subspace_count_per_point():
         _X, Y = DataLoader.load(dataset)
         outlier_num, inlier_num = np.sum(Y == 1), np.sum(Y == 0)
         feature_index = np.array([i for i in range(_X.shape[1])])
-        X_gpu_tensor = GKE_GPU.convert_to_tensor(_X)
-        # mdl = kNN(max(10, int(np.floor(0.03 * _X.shape[0]))), Normalize.ZSCORE)
-        mdl = GKE_GPU(Normalize.ZSCORE)
+        if model == "knn":
+            mdl = kNN(max(10, int(np.floor(0.03 * _X.shape[0]))), Normalize.ZSCORE)
+            X_gpu_tensor = _X
+        elif model == "gke":
+            mdl = GKE_GPU(Normalize.ZSCORE)
+            X_gpu_tensor = GKE_GPU.convert_to_tensor(_X)
 
         model_outputs = []
         for l in range(1, len(feature_index) + 1):
@@ -218,7 +222,7 @@ def subspace_count_per_point():
                 "inlier_median": np.median(inlier_subspaces),
             }
 
-    output_file = f"subspace_count_per_point.json"
+    output_file = f"{model}_subspace_count_per_point.json"
     with open(output_file, "w") as w:
         w.write(f"{json.dumps(outputs)}\n")
     logger.info(f"Output file {output_file}")
