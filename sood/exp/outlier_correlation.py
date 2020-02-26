98516bd4c687b107ca53cbcc24c61ecfd28c8db2
zicun cong <congzicun@gmail.com>
2020-02-25 16:01:00 -0800

    Compute outlier correlation in subspaces

--- sood/exp/outlier_correlation.py
+++ sood/exp/outlier_correlation.py
@@ -90,7 +90,7 @@ def outlier_subspace_jump_path():
                                                         reverse=True)
                 _tmp = [(len(x[1] & covered_outliers), len(x[1] - covered_outliers))  for x in _tmp]
                 print("1", _tmp)
-                print("2", [(len(x[1] & covered_outliers), len(x[1] - covered_outliers))  for x in subspace_to_outlier.items()])
+                print("2", [x[1]  for x in subspace_to_outlier.items()])
                 _outliers = set()
                 for j in subspace_to_outlier.values():
                     _outliers |= set(j)
